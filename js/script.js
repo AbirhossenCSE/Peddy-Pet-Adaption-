c148f2efad0806e6ddf3cc114a8687999988f7ef
Abir Hossen <abir.cse4@gmail.com>
2024-10-16 14:45:52 +0600

    modal

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const loadCategories = () => {
     .then((data) => displayCategories(data.categories))
     .catch((err) => console.log(err));
 }
+loadCategories();
 
 // {
 //     "id": 1,
@@ -18,7 +19,7 @@ const displayCategories = (categories) => {
 
         const buttonContainer = document.createElement('button');
         buttonContainer.innerHTML = `
-            <button id="category-btn-${item.id}" onclick="loadCategoryPets(${item.id})" class="btn category-btn">
+            <button id="category-btn-${item.category}" onclick="loadCategoryPets('${item.category}')" class="btn category-btn">
                ${item.category}
             </button>
         `
@@ -30,19 +31,34 @@ const displayCategories = (categories) => {
     
 }
 
-
+// Active button
+const removeActiveClass = ()=> {
+    const buttons = document.getElementsByClassName('category-btn');
+    console.log(buttons);
+    for (let btn of buttons) {
+        btn.classList.remove('active');
+    }
+}
 
 // Load categories pets
 const loadCategoryPets = (category) => {
-    // alert(id);
+    // alert(category)
     fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
     .then((res) => res.json())
-    .then((data1) => displayPets(data1.data))
+    .then((data) => {
+        // Active class remove
+        removeActiveClass();
+        // active class add
+        const activeBtn = document.getElementById(`category-btn-${category}`)
+        activeBtn.classList.add("active");
+        displayPets(data.data);
+    })
+    
     .catch((err) => console.log(err));
 }
 
 
-loadCategories();
+
 
 const loadPets = (searchText = "") => {
     fetch(`https://openapi.programming-hero.com/api/peddy/pets?title=${searchText}`)
@@ -61,8 +77,8 @@ const displayPets = (pets) => {
         petContainer.classList.remove("grid")
         petContainer.innerHTML = `
             <div class=" min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
-                <img src="./assets/icon.png"/>
-                <h2 class=" text-center text-xl font-bold">No Content Here</h2>
+                <img src="./images/error.webp" />
+                <h2 class=" text-center text-xl font-bold">No Information Available</h2>
             </div>
         `;
     }
@@ -75,24 +91,30 @@ const displayPets = (pets) => {
         const card = document.createElement('div');
         card.classList = "card card-compact";
         card.innerHTML = `
-            <figure class="h-[200px] relative">
+            <figure class="">
                 <img
                 src= ${pet.image} class="h-full w-full object-cover" alt="Shoes" />
 
                 
                 
             </figure>
-            <div class="px-0 py-2 flex gap-2">
+            <div class="px-0 py-2 w-full">
                 
                 <div>
-                    <h2 class="font-bold">${pet.breed}</h2>
+                    <h2 class="font-bold">${pet.pet_name}</h2>
                     <div class="items-center gap-2">
-                        <p class="text-gray-400">${'Birth Date', pet.date_of_birth}</p>
-                        <p class="text-gray-400">${pet.gender}</p>
-                        <p class="text-gray-400">${pet.price}</p>
+                        <p class="text-gray-400">Bread: ${pet.breed}</p>
+                        <p class="text-gray-400">Birth Date: ${pet.date_of_birth}</p>
+                        <p class="text-gray-400">Gender: ${pet.gender}</p>
+                        <p class="text-gray-400">Price: ${pet.price}</p>
                     
                     </div>
-                    <p><button onclick="loadDetails('${pet.price}')" class=" btn btn-sm btn-error">Details</button></p>
+                    <div class="flex justify-between items-center w-full p-3">
+                        
+                        <button class="btn btn-sm btn-error">Details</button>
+                        <button class="btn btn-sm btn-error mx-auto">Adopt</button>
+                        <button onclick="loadDetails(${pet.petId})" class="btn btn-sm btn-error">Details</button>
+                    </div>
                 </div>
             </div>
         `;
@@ -101,5 +123,37 @@ const displayPets = (pets) => {
     })
     
 }
+
+const loadDetails = async (petId) => {
+    const uri = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`;
+    const res = await fetch(uri);
+    const data = await res.json();
+    displayDetails(data.petData)
+    
+}
+
+const displayDetails = (petData) => {
+    console.log(petData);
+    const detailContainer = document.getElementById('modal-content');
+    detailContainer.innerHTML = `
+        <img src=${petData.image} />
+        <div class="px-0 py-2 w-full">
+            <div class="text-black ">
+                <h2 class="font-bold text-black text-2xl">${petData.pet_name}</h2>
+                <div class="items-center gap-2">
+                    <p class="text-gray-400">Bread: ${petData.breed}</p>
+                    <p class="text-gray-400">Birth Date: ${petData.date_of_birth}</p>
+                    <p class="text-gray-400">Gender: ${petData.gender}</p>
+                    <p class="text-gray-400">Price: ${petData.price}</p>
+                </div>
+            </div>
+        </div>
+        <p><span class="font-bold text-black">Details Information</span> <br> ${petData.pet_details}</p>
+    `;
+    
+    document.getElementById('customModal').showModal();
+}
+
+
 
 loadPets();
